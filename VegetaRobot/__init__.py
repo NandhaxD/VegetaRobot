764005210627a52ea87cd4edb45e090771026269
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-08 19:15:33 +0530

    Update __init__.py

--- VegetaRobot/__init__.py
+++ VegetaRobot/__init__.py
@@ -187,6 +187,7 @@ else:
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("SungJinwoo", API_ID, API_HASH)
+pbot = Client("Vegetapyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
