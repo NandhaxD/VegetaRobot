0f89b24eb27bdc951d8989beac46e970fa651bf7
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-09 22:55:15 +0530

    Update global_bans.py

--- VegetaRobot/modules/global_bans.py
+++ VegetaRobot/modules/global_bans.py
@@ -524,7 +524,7 @@ dispatcher.add_handler(UNGBAN_HANDLER)
 dispatcher.add_handler(GBAN_LIST)
 dispatcher.add_handler(GBAN_STATUS)
 
-__mod_name__ = "🛡ᴀɴᴛɪ-sᴘᴀᴍ"
+__mod_name__ = "🛡AntiSpam"
 __handlers__ = [GBAN_HANDLER, UNGBAN_HANDLER, GBAN_LIST, GBAN_STATUS]
 
 if STRICT_GBAN:  # enforce GBANS if this is set
