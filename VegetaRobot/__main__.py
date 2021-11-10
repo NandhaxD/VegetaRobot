36a886f4cc3e05d26dcdc22d2446d78edb707521
Hamburger-service <93368521+Hamburger-service@users.noreply.github.com>
2021-11-10 21:34:46 +0530

    Update __main__.py

--- VegetaRobot/__main__.py
+++ VegetaRobot/__main__.py
@@ -240,8 +240,8 @@ def start(update: Update, context: CallbackContext):
 
             elif args[0][1:].isdigit() and "rules" in IMPORTED:
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
-                
-            else:
+
+        else:
             update.effective_message.reply_sticker(
                 random.choice(STICKERS),
                 timeout=60,
