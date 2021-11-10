0a2b76e56bcf1d200a84556f5fbb0cc9ec7a836c
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-10 20:13:13 +0530

    Update __main__.py

--- VegetaRobot/__main__.py
+++ VegetaRobot/__main__.py
@@ -234,7 +234,12 @@ def start(update: Update, context: CallbackContext):
             elif args[0][1:].isdigit() and "rules" in IMPORTED:
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
 
-        else:
+       else:
+            update.effective_message.reply_sticker(
+                random.choice(STICKERS),
+                timeout=60,
+            )
+     else:
             update.effective_message.reply_text(
                 PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(buttons),
