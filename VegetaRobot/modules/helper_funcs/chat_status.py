d9d0c3b6f31357616566fbd944144007e53ef295
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-10 12:08:17 +0530

    Update chat_status.py

--- VegetaRobot/modules/helper_funcs/chat_status.py
+++ VegetaRobot/modules/helper_funcs/chat_status.py
@@ -32,6 +32,9 @@ def is_support_plus(chat: Chat, user_id: int, member: ChatMember = None) -> bool
 def is_sudo_plus(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     return user_id in DRAGONS or user_id in DEV_USERS
 
+def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
+    return chat.get_member(user.id).can_change_info
+
 
 def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if (
