ed84aa64075e1412456e224587afb79342939ccf
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-10 18:59:33 +0530

    Update admin.py

--- VegetaRobot/modules/admin.py
+++ VegetaRobot/modules/admin.py
@@ -123,14 +123,10 @@ def promote(update: Update, context: CallbackContext) -> str:
         bot.promoteChatMember(
             chat.id,
             user_id,
-            can_change_info=bot_member.can_change_info,
-            can_post_messages=bot_member.can_post_messages,
-            can_edit_messages=bot_member.can_edit_messages,
             can_delete_messages=bot_member.can_delete_messages,
             can_invite_users=bot_member.can_invite_users,
-            # can_promote_members=bot_member.can_promote_members,
-            can_restrict_members=bot_member.can_restrict_members,
-            can_pin_messages=bot_member.can_pin_messages)
+            can_pin_messages=bot_member.can_pin_messages,
+        )
     except BadRequest as err:
         if err.message == "User_not_mutual_contact":
             message.reply_text(
