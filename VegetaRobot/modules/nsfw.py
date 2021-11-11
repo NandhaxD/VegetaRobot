0dcf4863ffdc4554dbd219b892a391f5d1bc26f5
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-11 13:48:52 +0530

    Update nsfw.py

--- VegetaRobot/modules/nsfw.py
+++ VegetaRobot/modules/nsfw.py
@@ -242,17 +242,6 @@ def femdom(update, context):
     msg.reply_photo(nekos.img(target))
 
 @run_async
-def erok(update, context):
-    chat_id = update.effective_chat.id
-    if not update.effective_message.chat.type == "private":
-        is_nsfw = sql.is_nsfw(chat_id)
-        if not is_nsfw:
-            return
-    msg = update.effective_message
-    target = "erok"
-    msg.reply_photo(nekos.img(target))
-
-@run_async
 def smug(update, context):
     msg = update.effective_message
     target = "smug"
