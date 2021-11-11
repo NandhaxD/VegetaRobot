e358a093d3ed1bc39914584e194c5d70cfab4045
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-11 09:03:21 +0530

    Update nsfw.py

--- VegetaRobot/modules/nsfw.py
+++ VegetaRobot/modules/nsfw.py
@@ -82,17 +82,6 @@ def neko(update, context):
     msg.reply_photo(nekos.img(target))
 
 @run_async
-def cumgif(update, context):
-    chat_id = update.effective_chat.id
-    if not update.effective_message.chat.type == "private":
-        is_nsfw = sql.is_nsfw(chat_id)
-        if not is_nsfw:
-            return
-    msg = update.effective_message
-    target = "cum"
-    msg.reply_video(nekos.img(target))
-
-@run_async
 def erokemo(update, context):
     chat_id = update.effective_chat.id
     if not update.effective_message.chat.type == "private":
