66dcf8b17766b1604d8192467909239966056d42
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-11 09:07:37 +0530

    Update nsfw.py

--- VegetaRobot/modules/nsfw.py
+++ VegetaRobot/modules/nsfw.py
@@ -86,18 +86,7 @@ def wallpaper(update, context):
     msg = update.effective_message
     target = "wallpaper"
     msg.reply_photo(nekos.img(target))
-
-@run_async
-def lewdk(update, context):
-    chat_id = update.effective_chat.id
-    if not update.effective_message.chat.type == "private":
-        is_nsfw = sql.is_nsfw(chat_id)
-        if not is_nsfw:
-            return
-    msg = update.effective_message
-    target = "lewdk"
-    msg.reply_photo(nekos.img(target))
-
+    
 @run_async
 def ngif(update, context):
     chat_id = update.effective_chat.id
