6a66d59137f52bab5bd39a839cf63063e074d2a2
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-11 08:58:22 +0530

    Update nsfw.py

--- VegetaRobot/modules/nsfw.py
+++ VegetaRobot/modules/nsfw.py
@@ -80,18 +80,7 @@ def neko(update, context):
     msg = update.effective_message
     target = "neko"
     msg.reply_photo(nekos.img(target))
-
-@run_async
-def futanari(update, context):
-    chat_id = update.effective_chat.id
-    if not update.effective_message.chat.type == "private":
-        is_nsfw = sql.is_nsfw(chat_id)
-        if not is_nsfw:
-            return
-    msg = update.effective_message
-    target = "futanari"
-    msg.reply_photo(nekos.img(target))
-
+    
 @run_async
 def hololewd(update, context):
     chat_id = update.effective_chat.id
