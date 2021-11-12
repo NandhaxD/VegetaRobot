2bdb0e8c224281ab6dd76c888458e883b8e9d378
ValtAoiTheBot <91861970+ValtAoiTheBot@users.noreply.github.com>
2021-11-12 11:41:44 +0530

    Update __init__.py

--- VegetaRobot/__init__.py
+++ VegetaRobot/__init__.py
@@ -91,6 +91,7 @@ if ENV:
     WALL_API = os.environ.get('WALL_API', None)
     SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get('SPAMWATCH_SUPPORT_CHAT', None)
+    ARQ_API_URL = os.environ.get("ARQ_API_URL", None)
     SPAMWATCH_API = os.environ.get('SPAMWATCH_API', None)
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None) # From:- https://www.remove.bg/
     REPOSITORY = os.environ.get("REPOSITORY", "")
