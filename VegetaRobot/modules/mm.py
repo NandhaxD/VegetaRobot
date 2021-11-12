c715a050e3a3d01070121cfbcd0fdef4c68af5bc
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-12 14:42:02 +0530

    Update mm.py

--- VegetaRobot/modules/mm.py
+++ VegetaRobot/modules/mm.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import *
 
 from VegetaRobot.conf import get_str_key
-from VegetaRobot import pbot
+from VegetaRobot import pgram
 
 REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/980cd3c855f8e7c0bcdc9.jpg) to Make Your Groups Secured and Organized ! My friend : 『 [Free De la hoya](t.me/FreedelahoyaRobot) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @Pegasusupdates «««"
   
@@ -21,8 +21,8 @@ BUTTONS = InlineKeyboardMarkup(
     )
   
   
-@pbot.on_message(filters.command(["repo"]))
-async def repo(pbot, update):
+@pgram.on_message(filters.command(["repo"]))
+async def repo(pgram, update):
     await update.reply_text(
         text=REPO_TEXT,
         reply_markup=BUTTONS,
