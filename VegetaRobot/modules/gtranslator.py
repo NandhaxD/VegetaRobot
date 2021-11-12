0700d521ee3c077ae3cb31117b9c4289e18fd031
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-12 18:27:30 +0530

    Update gtranslator.py

--- VegetaRobot/modules/gtranslator.py
+++ VegetaRobot/modules/gtranslator.py
@@ -1,142 +1,95 @@
-from emoji import UNICODE_EMOJI
-from googletrans import LANGUAGES, Translator
-from VegetaRobot import dispatcher
-from VegetaRobot.modules.disable import DisableAbleCommandHandler
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, run_async
+"""
+MIT License
 
+Copyright (C) 2021 Awesome-RJ
 
-@run_async
-def totranslate(update: Update, context: CallbackContext):
-    msg = update.effective_message
-    problem_lang_code = []
-    for key in LANGUAGES:
-        if "-" in key:
-            problem_lang_code.append(key)
-    try:
-        if msg.reply_to_message:
-            args = update.effective_message.text.split(None, 1)
-            if msg.reply_to_message.text:
-                text = msg.reply_to_message.text
-            elif msg.reply_to_message.caption:
-                text = msg.reply_to_message.caption
+This file is part of @Cutiepii_Robot (Telegram Bot)
 
-            message = update.effective_message
-            dest_lang = None
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-            try:
-                source_lang = args[1].split(None, 1)[0]
-            except:
-                source_lang = "en"
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-            if source_lang.count('-') == 2:
-                for lang in problem_lang_code:
-                    if lang in source_lang:
-                        if source_lang.startswith(lang):
-                            dest_lang = source_lang.rsplit("-", 1)[1]
-                            source_lang = source_lang.rsplit("-", 1)[0]
-                        else:
-                            dest_lang = source_lang.split("-", 1)[1]
-                            source_lang = source_lang.split("-", 1)[0]
-            elif source_lang.count('-') == 1:
-                for lang in problem_lang_code:
-                    if lang in source_lang:
-                        dest_lang = source_lang
-                        source_lang = None
-                        break
-                if dest_lang is None:
-                    dest_lang = source_lang.split("-")[1]
-                    source_lang = source_lang.split("-")[0]
-            else:
-                dest_lang = source_lang
-                source_lang = None
-
-            exclude_list = UNICODE_EMOJI.keys()
-            for emoji in exclude_list:
-                if emoji in text:
-                    text = text.replace(emoji, '')
-
-            trl = Translator()
-            if source_lang is None:
-                detection = trl.detect(text)
-                tekstr = trl.translate(text, dest=dest_lang)
-                return message.reply_text(
-                    f"Translated from `{detection.lang}` to `{dest_lang}`:\n`{tekstr.text}`",
-                    parse_mode=ParseMode.MARKDOWN)
-            else:
-                tekstr = trl.translate(text, dest=dest_lang, src=source_lang)
-                message.reply_text(
-                    f"Translated from `{source_lang}` to `{dest_lang}`:\n`{tekstr.text}`",
-                    parse_mode=ParseMode.MARKDOWN)
-        else:
-            args = update.effective_message.text.split(None, 2)
-            message = update.effective_message
-            source_lang = args[1]
-            text = args[2]
-            exclude_list = UNICODE_EMOJI.keys()
-            for emoji in exclude_list:
-                if emoji in text:
-                    text = text.replace(emoji, '')
-            dest_lang = None
-            temp_source_lang = source_lang
-            if temp_source_lang.count('-') == 2:
-                for lang in problem_lang_code:
-                    if lang in temp_source_lang:
-                        if temp_source_lang.startswith(lang):
-                            dest_lang = temp_source_lang.rsplit("-", 1)[1]
-                            source_lang = temp_source_lang.rsplit("-", 1)[0]
-                        else:
-                            dest_lang = temp_source_lang.split("-", 1)[1]
-                            source_lang = temp_source_lang.split("-", 1)[0]
-            elif temp_source_lang.count('-') == 1:
-                for lang in problem_lang_code:
-                    if lang in temp_source_lang:
-                        dest_lang = None
-                        break
-                    else:
-                        dest_lang = temp_source_lang.split("-")[1]
-                        source_lang = temp_source_lang.split("-")[0]
-            trl = Translator()
-            if dest_lang is None:
-                detection = trl.detect(text)
-                tekstr = trl.translate(text, dest=source_lang)
-                return message.reply_text(
-                    "Translated from `{}` to `{}`:\n`{}`".format(
-                        detection.lang, source_lang, tekstr.text),
-                    parse_mode=ParseMode.MARKDOWN)
-            else:
-                tekstr = trl.translate(text, dest=dest_lang, src=source_lang)
-                message.reply_text(
-                    "Translated from `{}` to `{}`:\n`{}`".format(
-                        source_lang, dest_lang, tekstr.text),
-                    parse_mode=ParseMode.MARKDOWN)
-
-    except IndexError:
-        update.effective_message.reply_text(
-            "Reply to messages or write messages from other languages ​​for translating into the intended language\n\n"
-            "Example: `/tr en-ml` to translate from English to Malayalam\n"
-            "Or use: `/tr ml` for automatic detection and translating it into Malayalam.\n"
-            "See [List of Language Codes](t.me/OnePunchSupport/12823) for a list of language codes.",
-            parse_mode="markdown",
-            disable_web_page_preview=True)
-    except ValueError:
-        update.effective_message.reply_text(
-            "The intended language is not found!")
-    else:
-        return
-
-
-__help__ = """
-• `/tr` or `/tl` (language code) as reply to a long message
-*Example:* 
-  `/tr en`*:* translates something to english
-  `/tr hi-en`*:* translates hindi to english
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
-TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
+from gpytranslate import SyncTranslator
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext
+
+from VegetaRobot import dispatcher
+from VegetaRobot.modules.disable import DisableAbleCommandHandler
+
+trans = SyncTranslator()
+
+def translate(update: Update, context: CallbackContext) -> None:
+    bot = context.bot
+    message = update.effective_message
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        message.reply_text("Reply to a message to translate it!")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = trans.detect(to_translate)
+        dest = "en"
+    translation = trans(to_translate,
+                        sourcelang=source, targetlang=dest)
+    reply = f"<b>Translated from {source} to {dest}</b>:\n" \
+        f"<code>{translation.text}</code>"
+
+    bot.send_message(text=reply, chat_id=message.chat.id, parse_mode=ParseMode.HTML)
+
+
+def languages(update: Update, context: CallbackContext) -> None:
+    message = update.effective_message
+    bot = context.bot
+    bot.send_message(
+        text="Click [here](https://telegra.ph/Lang-Codes-03-19-3) to see the list of supported language codes!",
+        chat_id=message.chat.id, disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
+
+__help__ = """ 
+Use this module to translate stuff!
+
+*Commands:*
+   ➢ `/tl` (or `/tr`): as a reply to a message, translates it to English.
+   ➢ `/tl <lang>`: translates to <lang>
+eg: `/tl ja`: translates to Japanese.
+
+   ➢ `/tl <source>//<dest>`: translates from <source> to <lang>.
+eg: `/tl ja//en`: translates from Japanese to English.
+
+• [List of supported languages for translation](https://telegra.ph/Lang-Codes-03-19-3)
+"""
+
+TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], translate, run_async=True)
+TRANSLATE_LANG_HANDLER = DisableAbleCommandHandler(["lang", "languages"], languages, run_async=True)
 
 dispatcher.add_handler(TRANSLATE_HANDLER)
+dispatcher.add_handler(TRANSLATE_LANG_HANDLER)
 
 __mod_name__ = "Translator"
-__command_list__ = ["tr", "tl"]
-__handlers__ = [TRANSLATE_HANDLER]
+__command_list__ = ["tr", "tl", "lang", "languages"]
+__handlers__ = [TRANSLATE_HANDLER, TRANSLATE_LANG_HANDLER]
