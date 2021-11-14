670fe8471d50f947a4fc8ecada42c1af5b51e99b
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-14 10:38:39 +0530

    Update global_filter.py

--- VegetaRobot/modules/global_filter.py
+++ VegetaRobot/modules/global_filter.py
@@ -1,9 +1,19 @@
+import html
+import random
+import re
 
+import requests as r
+from telegram import MAX_MESSAGE_LENGTH, ParseMode, Update
+from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import escape_markdown
-from telegram import MAX_MESSAGE_LENGTH, ParseMode, Update
 
+import VegetaRobot.modules.fun_strings as fun
+from VegetaRobot import DEMONS, DRAGONS, dispatcher
+from VegetaRobot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
 from VegetaRobot.modules.helper_funcs.alternate import typing_action
+from VegetaRobot.modules.helper_funcs.extraction import extract_user
+
 
 @run_async
 @typing_action
