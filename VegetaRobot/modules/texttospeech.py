399750d14001144a492933e28d4a73e302d442f3
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-14 16:27:28 +0530

    Update texttospeech.py

--- VegetaRobot/modules/texttospeech.py
+++ VegetaRobot/modules/texttospeech.py
@@ -85,7 +85,6 @@ ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
 sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
 vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 """
- """
 
 __mod_name_"🗣TTS"
 
