b0e7b5bd21e18fbd9b9ab96fd6d54f588648d8ba
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-16 19:30:33 +0530

    Update telegraph.py

--- VegetaRobot/modules/telegraph.py
+++ VegetaRobot/modules/telegraph.py
@@ -12,7 +12,7 @@ r = telegraph.create_account(short_name=emilia)
 auth_url = r["auth_url"]
 
 
-@register(pattern="^/tg(m|xt) ?(.*)")
+@register(pattern="^/telegrap(h|xt) ?(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -21,7 +21,7 @@ async def _(event):
         start = datetime.now()
         r_message = await event.get_reply_message()
         input_str = event.pattern_match.group(1)
-        if input_str == "m":
+        if input_str == "h":
             downloaded_file_name = await tbot.download_media(
                 r_message,
                 TMP_DOWNLOAD_DIRECTORY
