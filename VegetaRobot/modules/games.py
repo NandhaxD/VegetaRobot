ed4e2bf4a22962a7fdb5f8c93706584390ef73f6
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-16 23:19:49 +0530

    Update games.py

--- VegetaRobot/modules/games.py
+++ VegetaRobot/modules/games.py
@@ -61,6 +61,26 @@ async def _(event):
                 r = await event.reply(file=InputMediaDice("🏀"))
         except BaseException:
             pass
+        
+@register(pattern="^/lol(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    r = await event.reply(file=InputMediaDice("😂"))
+    input_int = int(input_str)
+    if input_int > 5:
+        await event.reply("hey nigga use number 1 to 6 only")
+    
+    else:
+        try:
+            required_number = input_int
+            while r.media.value != required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice("😂"))
+        except BaseException:
+            pass
+
 
 __help__="""
 /dice or /dice 1 to 6 any value
