6c76e568a61cc2e3f37f59a1c52ca41a1832d091
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-16 19:19:36 +0530

    Update telegraph.py

--- VegetaRobot/modules/telegraph.py
+++ VegetaRobot/modules/telegraph.py
@@ -12,7 +12,7 @@ r = telegraph.create_account(short_name=emilia)
 auth_url = r["auth_url"]
 
 
-@register(pattern="^/t(m|xt) ?(.*)")
+@register(pattern="^/tg(m|xt) ?(.*)")
 async def _(event):
     if event.fwd_from:
         return
