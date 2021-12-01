251a51aa27e654c35c1ebbd8ac0953cf8425f1ca
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-01 22:41:54 +0530

    Update wish.py

--- VegetaRobot/modules/wish.py
+++ VegetaRobot/modules/wish.py
@@ -1,14 +1,13 @@
 from pyrogram import filters
 from VegetaRobot import pgram
 
+import VegetaRobot.modules.fun_strings as fun
+
 IMG= "https://telegra.ph/file/2148458205e9f278ed12c.jpg"
 
 @pgram.on_message(filters.command("hi"))
 async def hmm(_, message):
-    await message.reply_photo(IMG,random.choice(caption=
-                                                ("hi",
-                                                "bye",
-                                                 "mm" )
+    await message.reply_photo(random.choice(IMG,fun.WISH
                                                         
    ) 
     
