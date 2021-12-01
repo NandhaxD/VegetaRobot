a59ae77c5f8e867743f95ca00e3669bb4941bb99
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-01 23:38:52 +0530

    Update bans.py

--- VegetaRobot/modules/bans.py
+++ VegetaRobot/modules/bans.py
@@ -126,7 +126,7 @@ def ban(update: Update, context: CallbackContext) -> str:
             reply += f"\n<code> </code><b>• Reason:</b> \n{html.escape(reason)}"
 
         bot.send_file(
-            file=BAN_IMG
+            file=BAN_IMG,
             chat.id,
             reply,
             reply_markup=InlineKeyboardMarkup(
