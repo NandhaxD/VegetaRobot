6313e591fc3f52e5d0f5d836c8cea94d5164c898
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-01 23:09:14 +0530

    Update wish.py

--- VegetaRobot/modules/wish.py
+++ VegetaRobot/modules/wish.py
@@ -8,7 +8,8 @@ IMG= "https://telegra.ph/file/2148458205e9f278ed12c.jpg"
 
 @pgram.on_message(filters.command("wish"))
 async def hmm(_, message):
-    await message.reply_photo(random.choice(IMG,caption=fun.WISH)
+     mm = random.randint(1,100)
+    await message.reply_photo(IMG,caption=f"Your wish has been cast.✨**\n\n__chance of success {mm}%"
                                                         
    ) 
     
