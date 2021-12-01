a7ab8c452748be4c279ed8937cda8784be9a497a
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-01 19:04:11 +0530

    Update yourwish.py

--- VegetaRobot/modules/yourwish.py
+++ VegetaRobot/modules/yourwish.py
@@ -6,21 +6,19 @@ from telethon import events
 async def wish(vegeta):
    if vegeta.is_reply:
          mm = random.randint(1,100)
-<<<<<<< patch-4
          lol = await vegeta.get_reply_message()
          await tbot.send_message(vegeta.chat_id, f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=lol)
    if not vegeta.is_reply:
          mm = random.randint(1,100)
          VEGETA = "https://telegra.ph/file/68be46bb292a9230a584b.jpg"
          await tbot.send_file(vegeta.chat_id, VEGETA,caption=f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=vegeta)
-=======
          lol = await event.get_reply_message()
          await tbot.send_file(event.chat_id, f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=lol)
    if not event.is_reply:
          mm = random.randint(1,100)
          VEGETA = "https://telegra.ph/file/68be46bb292a9230a584b.jpg"
          await tbot.send_file(event.chat_id,f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=lol,file=VEGETA)
->>>>>>> Vegeta
+
         
    
         #thanks to @h0daka for image
