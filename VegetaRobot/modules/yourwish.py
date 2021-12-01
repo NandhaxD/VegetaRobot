28a5fce057e7bd6b41da9d1fe7591b1801ce9c7d
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-01 17:49:36 +0530

    Update yourwish.py

--- VegetaRobot/modules/yourwish.py
+++ VegetaRobot/modules/yourwish.py
@@ -1,6 +1,8 @@
 import random
 from VegetaRobot import telethn as asst
 from telethon import events
+
+VEGETA = "https://telegra.ph/file/68be46bb292a9230a584b.jpg"
 @asst.on(events.NewMessage(pattern="/wish"))
 async def wish(e):
    if e.is_reply:
@@ -9,6 +11,8 @@ async def wish(e):
          await asst.send_message(e.chat_id, f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=lol)
    if not e.is_reply:
          mm = random.randint(1,100)
-         await asst.send_message(e.chat_id, f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=e)
+         await asst.send_message(e.chat_id, VEGETA, f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=e)
         
-        #© @pegasusXteam
+   
+        #thanks to AASF for image
+      #Codes by @Tamilvip008
