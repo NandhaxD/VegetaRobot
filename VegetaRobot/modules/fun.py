76451a4eaeb1678b76f6fa06003c4df7a843932e
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-02 10:02:09 +0530

    Update fun.py

--- VegetaRobot/modules/fun.py
+++ VegetaRobot/modules/fun.py
@@ -82,7 +82,7 @@ def gbam(update, context):
 @run_async
 def decide(update: Update, context: CallbackContext):
     reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
-    reply_text(random.choice(DECIDE_IMG,fun.DECIDE))
+    reply_photo(random.choice(GN_IMG,fun.DECIDE))
     
 
 @run_async
