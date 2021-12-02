9e10548fbff251fe67d0fef5d4b3763b745401c0
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-02 13:34:34 +0530

    Update fun.py

--- VegetaRobot/modules/fun.py
+++ VegetaRobot/modules/fun.py
@@ -17,10 +17,6 @@ from VegetaRobot.modules.helper_funcs.alternate import typing_action
 from VegetaRobot.modules.helper_funcs.extraction import extract_user
 
 GN_IMG= "https://telegra.ph/file/6b67b1f44a669a634cab8.jpg"
-DECIDE_IMG= ( 
-     "https://telegra.ph/file/6b67b1f44a669a634cab8.jpg",
-     "https://telegra.ph/file/6b67b1f44a669a634cab8.jpg",
-     "https://telegra.ph/file/6b67b1f44a669a634cab8.jpg" )
 
 @run_async
 @typing_action
@@ -82,7 +78,7 @@ def gbam(update, context):
 @run_async
 @typing_action
 def decide(update: Update, context: CallbackContext):
-     reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
+    reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
     reply_text(random.choice(fun.DECIDE))
 
 @run_async
