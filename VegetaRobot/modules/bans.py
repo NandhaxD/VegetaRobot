71b392aebda7951678c21d30dbeb617161a12777
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-02 15:39:35 +0530

    Update bans.py

--- VegetaRobot/modules/bans.py
+++ VegetaRobot/modules/bans.py
@@ -306,7 +306,7 @@ def unbanb_btn(update: Update, context: CallbackContext) -> str:
             except BadRequest:
                 pass
             chat.unban_member(user_id)
-            query.message.edit_text("Yep, this user can join!")
+            query.message.edit_photo("Yep, this user can join!")
             bot.answer_callback_query(query.id, text="Unbanned!")
             return (
                 f"<b>{html.escape(chat.title)}:</b>\n"
