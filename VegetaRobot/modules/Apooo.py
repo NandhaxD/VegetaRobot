9942abbc9e6b0499ad6d2fb86a3cf4fc532376c8
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-03 22:06:28 +0530

    Update Apooo.py

--- VegetaRobot/modules/Apooo.py
+++ VegetaRobot/modules/Apooo.py
@@ -23,4 +23,4 @@ async def apakah(event):
     if not quew:
         await event.reply('Berikan saya pertanyaan 😐')
         return
-    await event.reply_photo(IDK_IMG,random.choice(APAKAH_STRING))
+    await event.reply_photo(IDK_IMG,random.choice(caption=APAKAH_STRING))
