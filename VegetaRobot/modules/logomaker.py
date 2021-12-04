810617542ee1f341748eedd39745eeae4820a404
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-04 07:30:16 +0530

    Update logomaker.py

--- VegetaRobot/modules/logomaker.py
+++ VegetaRobot/modules/logomaker.py
@@ -87,14 +87,13 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
     fname = "logo.png"
     img.save(fname, "png")
-    await telethn.send_file(event.chat_id, file=fname, caption = f"**Made by @VegetaRobot**",reply_markup=InlineKeyboardMarkup(
+    await telethn.send_file(event.chat_id, file=fname, caption = "**Made by @VegetaRobot**",reply_markup=InlineKeyboardMarkup(
                 [
                     [
-       InlineKeyboardButton(text="📢 Updates", url=f"@{UPDATES_CHANNEL}")
-                    ]
-                ]
-            ),
-           )
+       InlineKeyboardButton(text="📢 Updates", url=f"@{UPDATES_CHANNEL}"),
+    ],
+] 
+
     await pesan.delete()
     if os.path.exists(fname):
             os.remove(fname)
