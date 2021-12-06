56a09ba8a9e166067673034d6c1f650fcd183c9c
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-06 22:39:15 +0530

    Update userinfo.py

--- VegetaRobot/modules/userinfo.py
+++ VegetaRobot/modules/userinfo.py
@@ -24,7 +24,7 @@ from VegetaRobot.modules.sql.afk_sql import is_afk, check_afk_status
 from VegetaRobot.modules.sql.users_sql import get_user_num_chats
 from VegetaRobot.modules.helper_funcs.chat_status import sudo_plus
 from VegetaRobot.modules.helper_funcs.extraction import extract_user
-from VegetaRobot import telethn as SaitamaTelethonClient, TIGERS, DRAGONS, DEMONS
+from VegetaRobot import telethn as vegetaTelethonClient, TIGERS, DRAGONS, DEMONS
 
 
 def no_by_per(totalhp, percentage):
@@ -151,7 +151,7 @@ def get_id(update: Update, context: CallbackContext):
                 parse_mode=ParseMode.HTML)
 
 
-@SaitamaTelethonClient.on(
+@vegetaTelethonClient.on(
     events.NewMessage(
         pattern='/ginfo ',
         from_users=(TIGERS or []) + (DRAGONS or []) + (DEMONS or [])))
