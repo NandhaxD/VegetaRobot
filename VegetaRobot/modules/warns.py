ab7d3e9c6054714f213d2e8d55fe3e0e88abc311
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-06 22:44:13 +0530

    Update warns.py

--- VegetaRobot/modules/warns.py
+++ VegetaRobot/modules/warns.py
@@ -86,7 +86,7 @@ def warn(user: User,
         for warn_reason in reasons:
             reply += f"\n - {html.escape(warn_reason)}"
 
-        # message.bot.send_sticker(chat.id, BAN_STICKER)  # Saitama's sticker
+        # message.bot.send_sticker(chat.id, BAN_STICKER)  # vegeta's sticker
         keyboard = None
         log_reason = (f"<b>{html.escape(chat.title)}:</b>\n"
                       f"#WARN_BAN\n"
