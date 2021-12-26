28afc5f8b8cbefa16a55360e20a212d65a4e34e1
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-26 09:06:56 +0530

    Update logomaker.py

--- VegetaRobot/modules/logomaker.py
+++ VegetaRobot/modules/logomaker.py
@@ -90,7 +90,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
     fname = "logo.png"
     img.save(fname, "png")
-    await telethn.send_file(event.chat_id, file=fname, caption ="**Made by @VegetaRobot**",Button.url("Support", f"@{SUPPORT_CHAT}")
+    await telethn.send_file(event.chat_id, file=fname, caption ="**Made by @VegetaRobot**")
     
     await pesan.delete()
     if os.path.exists(fname):
