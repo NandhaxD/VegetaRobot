75dee0ec3441144604b8571839779b732294bda9
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-27 21:23:58 +0530

    Update config.py

--- VegetaRobot/config.py
+++ VegetaRobot/config.py
@@ -53,8 +53,8 @@ class Config(object):
     WORKERS = 8  # Number of subthreads to use. Set as number of threads your processor uses
     BAN_STICKER = ''  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
-    ARQ_API_URL = ""
-    ARQ_API_KEY = ""
+    ARQ_API_URL = "https://thearq.tech"
+    ARQ_API_KEY = "YIECCC-NAJARO-OLLREW-SJSRIP-ARQ"
     CASH_API_KEY = 'awoo'  # Get your API key from https://www.alphavantage.co/support/#api-key
     TIME_API_KEY = 'awoo'  # Get your API key from https://timezonedb.com/api
     OPENWEATHERMAP_ID = 'awoo'
