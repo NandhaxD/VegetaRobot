30edcc0764cc3b461a3970d1bfda6fdd12026fdd
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-28 12:22:10 +0530

    Update decorators.py

--- VegetaRobot/modules/helper_funcs/decorators.py
+++ VegetaRobot/modules/helper_funcs/decorators.py
@@ -11,7 +11,7 @@ class KigyoTelegramHandler:
 
     def command(
             self, command: str, filters: Optional[BaseFilter] = None, admin_ok: bool = False, pass_args: bool = False,
-            pass_chat_data: bool = False, run_async: bool = True, can_disable: bool = True,
+            pass_chat_data: bool = False, bool = True, can_disable: bool = True,
             group: Optional[int] = 40
     ):
 
@@ -19,23 +19,23 @@ class KigyoTelegramHandler:
             try:
                 if can_disable:
                     self._dispatcher.add_handler(
-                        DisableAbleCommandHandler(command, func, filters=filters, run_async=run_async,
+                        DisableAbleCommandHandler(command, func, filters=filters, 
                                                   pass_args=pass_args, admin_ok=admin_ok), group
                     )
                 else:
                     self._dispatcher.add_handler(
-                        CommandHandler(command, func, filters=filters, run_async=run_async, pass_args=pass_args), group
+                        CommandHandler(command, func, filters=filters, pass_args=pass_args), group
                     )
                 log.debug(f"[KIGCMD] Loaded handler {command} for function {func.__name__} in group {group}")
             except TypeError:
                 if can_disable:
                     self._dispatcher.add_handler(
-                        DisableAbleCommandHandler(command, func, filters=filters, run_async=run_async,
+                        DisableAbleCommandHandler(command, func, filters=filters,
                                                   pass_args=pass_args, admin_ok=admin_ok, pass_chat_data=pass_chat_data)
                     )
                 else:
                     self._dispatcher.add_handler(
-                        CommandHandler(command, func, filters=filters, run_async=run_async, pass_args=pass_args,
+                        CommandHandler(command, func, filters=filters, pass_args=pass_args,
                                        pass_chat_data=pass_chat_data)
                     )
                 log.debug(f"[KIGCMD] Loaded handler {command} for function {func.__name__}")
@@ -50,21 +50,21 @@ class KigyoTelegramHandler:
             try:
                 if can_disable:
                     self._dispatcher.add_handler(
-                        DisableAbleMessageHandler(pattern, func, friendly=friendly, run_async=run_async), group
+                        DisableAbleMessageHandler(pattern, func, friendly=friendly), group
                     )
                 else:
                     self._dispatcher.add_handler(
-                        MessageHandler(pattern, func, run_async=run_async), group
+                        MessageHandler(pattern, fun), group
                     )
                 log.debug(f"[KIGMSG] Loaded filter pattern {pattern} for function {func.__name__} in group {group}")
             except TypeError:
                 if can_disable:
                     self._dispatcher.add_handler(
-                        DisableAbleMessageHandler(pattern, func, friendly=friendly, run_async=run_async)
+                        DisableAbleMessageHandler(pattern, func, friendly=friendly)
                     )
                 else:
                     self._dispatcher.add_handler(
-                        MessageHandler(pattern, func, run_async=run_async)
+                        MessageHandler(pattern, func)
                     )
                 log.debug(f"[KIGMSG] Loaded filter pattern {pattern} for function {func.__name__}")
 
@@ -72,19 +72,19 @@ class KigyoTelegramHandler:
 
         return _message
 
-    def callbackquery(self, pattern: str = None, run_async: bool = True):
+    def callbackquery(self, pattern: str = None, bool = True):
         def _callbackquery(func):
-            self._dispatcher.add_handler(CallbackQueryHandler(pattern=pattern, callback=func, run_async=run_async))
+            self._dispatcher.add_handler(CallbackQueryHandler(pattern=pattern, callback=func))
             log.debug(f'[KIGCALLBACK] Loaded callbackquery handler with pattern {pattern} for function {func.__name__}')
             return func
 
         return _callbackquery
 
-    def inlinequery(self, pattern: Optional[str] = None, run_async: bool = True, pass_user_data: bool = True,
+    def inlinequery(self, pattern: Optional[str] = None,  bool = True, pass_user_data: bool = True,
                     pass_chat_data: bool = True, chat_types: List[str] = None):
         def _inlinequery(func):
             self._dispatcher.add_handler(
-                InlineQueryHandler(pattern=pattern, callback=func, run_async=run_async, pass_user_data=pass_user_data,
+                InlineQueryHandler(pattern=pattern, callback=func, pass_user_data=pass_user_data,
                                    pass_chat_data=pass_chat_data, chat_types=chat_types))
             log.debug(
                 f'[KIGINLINE] Loaded inlinequery handler with pattern {pattern} for function {func.__name__} | PASSES '
