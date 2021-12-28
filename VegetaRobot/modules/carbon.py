b4fa0c419ad710767be30285a16c2a9efe61f048
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-12-28 07:49:16 +0530

    Update carbon.py

--- VegetaRobot/modules/carbon.py
+++ VegetaRobot/modules/carbon.py
@@ -9,12 +9,13 @@ from VegetaRobot.utils.make_carbon import make_carbon
 @capture_err
 async def carbon_func(_, message):
     if not message.reply_to_message:
-        return await message.reply_text("**Reply to a text message to make carbon.**")
+        return await message.reply_text("**🙄Reply to a text message to make carbon.**")
     if not message.reply_to_message.text:
-        return await message.reply_text("**Reply to a text message to make carbon.**")
-    m = await message.reply_text("⬇Downloading...")
+        return await message.reply_text("**🙄Reply to a text message to make carbon.**")
+    m = await message.reply_text("**⬇Downloading...**")
     carbon = await make_carbon(message.reply_to_message.text)
-    await m.edit("***⬆Uploading...**")
-    await pbot.send_photo(message.chat.id, carbon)
+    await m.edit("**⬆Uploading...**")
+    msg = "**Made by @VegetaRobot**"
+    await pbot.send_photo(message.chat.id, caption=msg, carbon)
     await m.delete()
     carbon.close()
