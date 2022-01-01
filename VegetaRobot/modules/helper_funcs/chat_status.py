db3a0083e9bf019338e4d232056a29494decddd5
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-01 12:02:21 +0530

    Update chat_status.py

--- VegetaRobot/modules/helper_funcs/chat_status.py
+++ VegetaRobot/modules/helper_funcs/chat_status.py
@@ -38,6 +38,9 @@ def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
 def user_can_pin(chat: Chat, user: User, bot_id: int) -> bool:
     return chat.get_member(user.id).can_pin_messages
 
+def can_manage_voice_chats(chat: Chat, user: User, bot_id: int) -> bool: 
+    return chat.get_member(user.id).can_manage_voicechats
+
 
 def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if (
