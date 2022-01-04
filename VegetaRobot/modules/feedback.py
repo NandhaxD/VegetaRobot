3bc1f800ba2e6ed6da0a97606fa436f28c9447cd
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-05 00:26:09 +0530

    Create feedback.py

---
+++ VegetaRobot/modules/feedback.py
@@ -0,0 +1,44 @@
+import html
+from telegram import Update, Bot, ParseMode
+from telegram.ext import run_async
+from VegetaRobot.modules.disable import DisableAbleCommandHandler
+from VegetaRobot import dispatcher, SUPPORT_CHAT
+from requests import get
+from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+@run_async
+def feedback(bot: Bot, update: Update):
+  name = update.effective_message.from_user.first_name
+  message = update.effective_message
+  userid=message.from_user.id
+  text = message.text[len('/feedback '):]
+   
+
+  feed_text = f"Buddy's *New* feedback from [{name}](tg://user?id={userid})\n\nfeed: {text}"
+  
+
+  bot.send_message(@{SUPPORT_CHAT}, feed_text, parse_mode=ParseMode.MARKDOWN)
+ 
+  text = html.escape(text)
+  reply_text="Thank you for giving us your feedback."
+  message.reply_text(reply_text, reply_markup=InlineKeyboardMarkup(
+                                                [[InlineKeyboardButton(text="You can see your feedback here",url=f"https://t.me/{SUPPORT_CHAT})]]
+                                               
+  
+
+  
+
+
+
+__help__ = """
+ - /feedback : You can give us your feedbacks 
+               can can see your feeds here.
+"""
+
+__mod_name__ = "FEEDBACK🗣️"
+
+feed_handle = DisableAbleCommandHandler("feedback", feedback)
+
+dispatcher.add_handler(feed_handle)
