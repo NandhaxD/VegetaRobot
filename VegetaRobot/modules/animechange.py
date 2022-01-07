6d6e137ba9fbe94c956c19143500fae96b172e60
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-07 08:17:18 +0530

    Create animechange.py

---
+++ VegetaRobot/modules/animechange.py
@@ -0,0 +1,27 @@
+import requests
+from nksama import bot
+from pyrogram import filters
+from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
+from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
+from n.plugins.helpers import call_back_in_filter
+
+
+@bot.on_callback_query(call_back_in_filter('quotek'))
+def callback_quotek(_, query):
+    if query.data.split(":")[1] == "change":
+        #         query.message.delete()
+        kk = requests.get('https://animechan.vercel.app/api/random').json()
+        anime = kk['anime']
+        quote = kk['quote']
+        character = kk['character']
+        caption = f"""
+**Anime:** `{anime}`
+**Character:** `{character}`
+**Quote:** `{quote}`"""
+        query.message.edit(caption,
+                           reply_markup=InlineKeyboardMarkup([
+                               [
+                                   InlineKeyboardButton(
+                                       "Change", callback_data="quotek:change")
+                               ],
+                           ]))
