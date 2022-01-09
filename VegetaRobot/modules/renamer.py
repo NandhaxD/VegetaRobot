983ec6a57821f500e3373d9a40852ff548f175a5
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-09 08:43:52 +0530

    Create renamer.py

---
+++ VegetaRobot/modules/renamer.py
@@ -0,0 +1,22 @@
+from VegetaRobot import pgram as bot
+import os
+from nksama.utils.sendlog import send_log
+from pyrogram import filters
+
+
+@bot.on_message(filters.command('rename'))
+def rename(_, message):
+
+    try:
+        filename = message.text.replace(message.text.split(" ")[0], "")
+
+    except Exception as e:
+        send_log(e)
+
+    reply = message.reply_to_message
+    if reply:
+        x = message.reply_text("Downloading.....")
+        path = reply.download(file_name=filename)
+        x.edit("Uploading.....")
+        message.reply_document(path)
+        os.remove(path)
