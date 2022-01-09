4d63872e1e1f5473651cdb80ea3169497ef3753c
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-09 22:01:08 +0530

    Update info.py

--- VegetaRobot/modules/info.py
+++ VegetaRobot/modules/info.py
@@ -1,26 +1,4 @@
-"""
-MIT License
 
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 import os
 
 from pyrogram import filters
@@ -28,13 +6,7 @@ from pyrogram.types import Message
 
 from VegetaRobot import DRAGONS as SUDOERS, pgram as app
 from VegetaRobot.utils.sections import section
-from wbb.utils.dbfunctions import is_gbanned_user, user_global_karma
 
-__MODULE__ = "Info"
-__HELP__ = """
-/info [USERNAME|ID] - Get info about a user.
-/chat_info [USERNAME|ID] - Get info about a chat.
-"""
 
 
 async def get_user_info(user, already=False):
