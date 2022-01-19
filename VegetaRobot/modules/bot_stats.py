4edb236a4a96fb3ff4a6a62e540f323b2e88125b
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-19 07:29:57 +0530

    Create bot_stats.py

---
+++ VegetaRobot/modules/bot_stats.py
@@ -0,0 +1,40 @@
+
+import asyncio
+import os
+import subprocess
+import time
+
+import psutil
+from pyrogram import filters
+
+from VegetaRobot import (StartTime, DEV_USERS, pgram)
+import VegetaRobot.utils.formatter as formatter
+import VegetaRobot.modules.sql.users_sql as sql
+
+
+
+
+# Stats Module
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - StartTime)
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    process = psutil.Process(os.getpid())
+    users = sql.num_users()
+    chats = sql.num_chats()
+    stats = f"""
+➢ Vegeta Current System Stats
+
+────────────────────────
+• UPTIME: {formatter.get_readable_time((bot_uptime))}
+• BOT: {round(process.memory_info()[0] / 1024 ** 2)} MB
+• CPU: {cpu}%
+• RAM: {mem}%
+• DISK: {disk}%
+• CHATS: {chats}
+• USERS: {users}
+────────────────────────
+"""
+    return stats
