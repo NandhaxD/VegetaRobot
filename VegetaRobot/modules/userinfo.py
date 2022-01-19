9a64641446291120a02e7390f7afdf2f28b94995
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-19 19:49:52 +0530

    Update userinfo.py

--- VegetaRobot/modules/userinfo.py
+++ VegetaRobot/modules/userinfo.py
@@ -340,13 +340,12 @@ def info(update: Update, context: CallbackContext):
     if INFOPIC:
         try:
             profile = context.bot.get_user_profile_photos(user.id).photos[0][-1]
-            _file = bot.get_file(profile["file_id"])
-            _file.download(f"{user.id}.png")
-
-            message.reply_document(
-                document=open(f"{user.id}.png", "rb"),
-                caption=(text),
-                reply_markup=InlineKeyboardMarkup(
+            context.bot.sendChatAction(chat.id, "upload_photo")
+            context.bot.send_photo(
+            chat.id,
+            photo=profile,
+            caption=(text),
+            reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton(
@@ -356,10 +355,8 @@ def info(update: Update, context: CallbackContext):
                         ],
                     ]
                 ),
-                parse_mode=ParseMode.HTML,
+                parse_mode=ParseMode.HTML,disable_web_page_preview=True,
             )
-
-            os.remove(f"{user.id}.png")
         # Incase user don't have profile pic, send normal text
         except IndexError:
             message.reply_text(
