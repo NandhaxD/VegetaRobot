e115831890d93216e2add827972251784b9d8ac4
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-22 08:09:57 +0530

    Update rss_sql.py

--- VegetaRobot/modules/sql/rss_sql.py
+++ VegetaRobot/modules/sql/rss_sql.py
@@ -3,10 +3,13 @@ import threading
 from VegetaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Column, Integer, UnicodeText
 
+from sqlalchemy.sql.sqltypes import BigInteger
+
+
 
 class RSS(BASE):
     __tablename__ = "rss_feed"
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger, primary_key=True)
     chat_id = Column(UnicodeText, nullable=False)
     feed_link = Column(UnicodeText)
     old_entry_link = Column(UnicodeText)
