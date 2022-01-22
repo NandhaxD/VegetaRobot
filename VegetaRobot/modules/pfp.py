e8d79b0344ffd7b062aba2d14effd768d5b8d16d
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-22 20:46:13 +0530

    Create pfp.py

---
+++ VegetaRobot/modules/pfp.py
@@ -0,0 +1,47 @@
+
+import os
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from VegetaRobot import pgram as app
+from VegetaRobot.utils.sections import section
+
+
+async def get_user_info(user, already=False):
+    if not already:
+        user = await app.get_users(user)
+    if not user.first_name:
+        return ["Deleted account", None]
+    photo_id = user.photo.big_file_id if user.photo else None
+    body = {
+        "user pfp",
+    }
+    caption = section("User info", body)
+    return [caption, photo_id]
+
+
+@app.on_message(filters.command("info"))
+async def info_func(_, message: Message):
+    if message.reply_to_message:
+        user = message.reply_to_message.from_user.id
+    elif not message.reply_to_message and len(message.command) == 1:
+        user = message.from_user.id
+    elif not message.reply_to_message and len(message.command) != 1:
+        user = message.text.split(None, 1)[1]
+
+    m = await message.reply_text("Processing")
+
+    try:
+        info_caption, photo_id = await get_user_info(user)
+    except Exception as e:
+        return await m.edit(str(e))
+
+    if not photo_id:
+        return await m.edit(info_caption, disable_web_page_preview=True)
+    photo = await app.download_media(photo_id)
+
+    await message.reply_photo(photo, caption=info_caption, quote=False)
+    await m.delete()
+    os.remove(photo)
+
