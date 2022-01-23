17f5844d620ca66e962c8cf2751a974ed87f3002
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-23 20:06:01 +0530

    Update alive.py

--- VegetaRobot/modules/alive.py
+++ VegetaRobot/modules/alive.py
@@ -75,11 +75,10 @@ def alive(update: Update, context: CallbackContext):
         msg.edit_text(alive_you[x%7])
         time.sleep(EDIT_SLEEP)
     update.effective_message.reply_video(
-            INFO_IMG, caption=f"""\n*MASTER* - @bakugoNo1
+            INFO_IMG, caption=f"""*MASTER* - @bakugoNo1
                                   *Botstats* - `Alive`
                                   *ptb version - `{ptbver}`
                                   *telethon* - `{telever}`
-                                  *python*   - `{python_version}`
                                   *program* - `{pyrover}`""",reply_markup=InlineKeyboardMarkup(
                 [
                     [
