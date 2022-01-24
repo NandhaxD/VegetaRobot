ba0a4d1b43d1918245845c5990f0e02396f2bffd
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-24 17:36:42 +0530

    Update notes.py

--- VegetaRobot/modules/notes.py
+++ VegetaRobot/modules/notes.py
@@ -549,30 +549,25 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
-❂ /get <notename>*:* get the note with this notename
-❂ #<notename>*:* same as /get
-❂ /notes or /saved*:* list all saved notes in this chat
-❂ /number *:* Will pull the note of that number in the list
-If you would like to retrieve the contents of a note without any formatting, use `/get <notename> noformat`. This can \
-be useful when updating a current note
+*Admins Only:*
+✪ /save: <text> | reply_msg | save notes. 
+✪ /clear: <Notename> | notes delete. 
+✪ /notes: Show list of Notes in chat. 
+✪ /removeallnotes: remove all notes in chat.
+✪ /save: <text> | reply_msg | save notes. 
+✪ /clear: <Notename> | notes delete. 
+✪ /notes: Show list of Notes in chat. 
+✪ /removeallnotes: remove all notes in chat.
+✪ /get <notename>: get note. 
+✪ /save: <text> | reply_msg | save notes. 
+✪ /clear: <Notename> | notes delete. 
+✪ /notes: Show list of Notes in chat. 
+✪ /removeallnotes: remove all notes in chat.
+✪ /get <notename>: get note.
+✪ /<notenumber>: get note. 
+✪ #<notename>: get note. 
 
-*Admins only:*
-❂ /save <notename> <notedata>*:* saves notedata as a note with name notename
-A button can be added to a note by using standard markdown link syntax - the link should just be prepended with a \
-`buttonurl:` section, as such: `[somelink](buttonurl:example.com)`. Check `/markdownhelp` for more info
-❂ /save <notename>*:* save the replied message as a note with name notename
- Separate diff replies by `%%%` to get random notes
- *Example:*
- `/save notename
- Reply 1
- %%%
- Reply 2
- %%%
- Reply 3`
-❂ /clear <notename>*:* clear note with this name
-❂ /removeallnotes*:* removes all notes from the group
-
- *Note:* Note names are case-insensitive, and they are automatically converted to lowercase before getting saved.
+✪ button add in notes for more details : /markdownhelp
 """
 
 __mod_name__ = "📓Notes"
