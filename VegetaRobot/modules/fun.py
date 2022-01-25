9804de5b20fcd722b9e3da24ac311ed931d7171e
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-25 13:33:52 +0530

    Update fun.py

--- VegetaRobot/modules/fun.py
+++ VegetaRobot/modules/fun.py
@@ -109,7 +109,7 @@ def pfp(update, context):
     user = update.effective_user
     chat = update.effective_chat
     text="*Made by @VegetaRobot*"
-    profile = context.bot.get_user_profile_photos(user.id).photos[0][-4]
+    profile = context.bot.get_user_profile_photos(user.id).photos[0][-1]
     context.bot.sendChatAction(chat.id, "upload_photo")
     context.bot.send_photo(chat.id, photo=profil, ecaption=text,parse_mode=ParseMode.MARKDOWN)
 
