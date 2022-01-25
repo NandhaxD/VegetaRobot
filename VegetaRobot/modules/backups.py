d0219763b84daaff9d2ae8eb059816a1818fb36b
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-25 08:39:45 +0530

    Update backups.py

--- VegetaRobot/modules/backups.py
+++ VegetaRobot/modules/backups.py
@@ -363,15 +363,15 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "📦Backups"
+__mod_name__ = "ʙᴀᴄᴋᴜᴘ"
 
 __help__ = """
 *Only for group owner:*
 
- • /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
+ ✪︎ /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
  Note that files / photos cannot be imported due to telegram restrictions.
 
- • /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
+ ✪︎ /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
 
 """
 
