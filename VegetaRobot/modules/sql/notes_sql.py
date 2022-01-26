ab29aaf66cc07c630e974e3ae93c68aae848c439
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-26 09:20:49 +0530

    Update notes_sql.py

--- VegetaRobot/modules/sql/notes_sql.py
+++ VegetaRobot/modules/sql/notes_sql.py
@@ -1,4 +1,4 @@
-# Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
+# Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database..
 import threading
 
 from VegetaRobot.modules.helper_funcs.msg_types import Types
