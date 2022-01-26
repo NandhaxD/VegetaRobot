42e7fd06400e2780bea13b5b52b03fdfe386e83a
Hodaka <90975856+h0daka@users.noreply.github.com>
2022-01-26 13:19:43 +0530

    By @h0daka

--- VegetaRobot/modules/sql/warns_sql.py
+++ VegetaRobot/modules/sql/warns_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from KURUMIBOT.modules.sql import BASE, SESSION
+from VegetaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, String, UnicodeText, distinct, func, Integer
 from sqlalchemy.sql.sqltypes import BigInteger
 from sqlalchemy.dialects import postgresql
