f93c108b955e2d374a835c42e16360e990f4868d
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-01-26 13:55:07 +0530

    Update config.py

--- VegetaRobot/config.py
+++ VegetaRobot/config.py
@@ -27,9 +27,7 @@ class Config(object):
     STRING_SESSION = "1BVtsOIgBu2dMF7FyBJZOig7ITZDtkaJQ-9y2i_kNIP_zTcUQ5-QG-yl04x5jbZgsnIF1n8mVg8WVQbMZB8Hi5edGcQUy8NM9QXj5IILEeIHtEBs_dFMcZ0z-wKEZ4iL2tvbNyQOIqWAYGjnsR3c_-qmZCn3gnEjOWWN9HxLe3_6C7JVw2rhuVQVAR4dWWtVEjxWmnvUL32Dlp45STq92rBbzHyHhEUOMb6CJUKh7-b-1yS8vi7Yc_7KkL2ls8QFGg1s7i-paDZqZFnJrN0IyLj5WK01ZirX62Mqw7z9Ii58CETCQutPBU29Kwyp28vtvNjegZcANwgqZZN38vJ0G9TzwJTSX920="
     TEMP_DOWNLOAD_DIRECTORY = ""
     EVENT_LOGS = -1001543354286  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:F5m2YTn38DWCu9HBfc6i@containers-us-west-23.railway.app:5884/railway'
-    #'postgres://yjbklqzqytefhl:4a729e337257be81fdc359cd856e34a0b5a261419d5138f25b81193bb27774e8@ec2-52-54-38-229.compute-1.amazonaws.com:5432/d8pnbft2italbv'
-    #'postgres://vagszglpwcqjuv:7ac64f23996ef264ec3b67c65f3a6eaa1ac41a77588553e09623558127ff6d1f@ec2-52-202-198-60.compute-1.amazonaws.com:5432/d7pmn914up6sq4'
+    SQLALCHEMY_DATABASE_URI = ''
     LOAD = []
     NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
     WEBHOOK = None
@@ -57,7 +55,7 @@ class Config(object):
     BAN_STICKER = ''  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     ARQ_API_URL = "https://thearq.tech"
-    ARQ_API_KEY = "YIECCC-NAJARO-OLLREW-SJSRIP-ARQ"
+    ARQ_API_KEY = "FEUAAQ-IYDNKK-VTYUKD-LMMXLA-ARQ"
     CASH_API_KEY = 'awoo'  # Get your API key from https://www.alphavantage.co/support/#api-key
     TIME_API_KEY = 'awoo'  # Get your API key from https://timezonedb.com/api
     OPENWEATHERMAP_ID = 'awoo'
