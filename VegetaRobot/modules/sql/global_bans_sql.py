82101fdc33ec6a80e8a12bf66616f61c238af64e
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-01-26 19:42:27 +0530

    Update global_bans_sql.py

--- VegetaRobot/modules/sql/global_bans_sql.py
+++ VegetaRobot/modules/sql/global_bans_sql.py
@@ -1,7 +1,7 @@
 import threading
 
 from VegetaRobot.modules.sql import BASE, SESSION
-from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
+from sqlalchemy import Boolean, Column, BigInteger, String, UnicodeText
 
 from sqlalchemy.sql.sqltypes import BigInteger
 
