7886eb62fa75d633578aae1ed4f6b53a3747feae
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-09 00:33:24 +0530

    Update chatbot_sql.py

--- VegetaRobot/modules/sql/chatbot_sql.py
+++ VegetaRobot/modules/sql/chatbot_sql.py
@@ -1,7 +1,7 @@
 import threading
 
 from sqlalchemy import Column, String
-from ZeusXRobot.modules.sql import BASE, SESSION
+from VegetaRobot.modules.sql import BASE, SESSION
 
 class KukiChats(BASE):
     __tablename__ = "kuki_chats"
