d9ca0c2ace207f662ed256fa229cffb11ffe92db
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-09 00:30:02 +0530

    Update __init__.py

--- VegetaRobot/modules/sql/__init__.py
+++ VegetaRobot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from ZeusXRobot import DB_URI
+from VegetaRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
