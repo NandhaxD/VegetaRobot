fbefee2bf08c7b460719caf9175b0e6c6e89ac3e
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-09 00:35:06 +0530

    Update connection_sql.py

--- VegetaRobot/modules/sql/connection_sql.py
+++ VegetaRobot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, BigInteger
 
-from ZeusXRobot.modules.sql import SESSION, BASE
+from VegetaRobot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
