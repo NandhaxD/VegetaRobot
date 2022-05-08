1818b22daef2facd3c142fe2c3f98be3f40b98a8
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-09 00:38:13 +0530

    Update locks_sql.py

--- VegetaRobot/modules/sql/locks_sql.py
+++ VegetaRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from ZeusXRobot.modules.sql import SESSION, BASE
+from VegetaRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
