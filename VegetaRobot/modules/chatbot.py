a7321cd873264771aee0a0d87f5a7bb69dc8cd6f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-23 12:50:03 +0530

    Update chatbot.py

--- VegetaRobot/modules/chatbot.py
+++ VegetaRobot/modules/chatbot.py
@@ -39,7 +39,7 @@ def kukirm(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Zeus Chatbot disable by {}.".format(mention_html(user.id, user.first_name)),
+                "Chatbot disable by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
@@ -64,7 +64,7 @@ def kukiadd(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Zeus Chatbot enable by {}.".format(mention_html(user.id, user.first_name)),
+                "Chatbot enable by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
