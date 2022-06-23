34a55e2440ddda3d6bcfceb9a15a0f9d36a60048
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-23 14:25:01 +0530

    Update misc.py

--- VegetaRobot/modules/misc.py
+++ VegetaRobot/modules/misc.py
@@ -105,7 +105,7 @@ MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help)
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 
-__mod_name__ = "🌠Extras"
+__mod_name__ = "Extras"
 __command_list__ = ["id", "echo"]
 __handlers__ = [
     ECHO_HANDLER,
