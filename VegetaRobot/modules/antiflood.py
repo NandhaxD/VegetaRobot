680fa4d2caffcc02d94a76e35df6a7e306a8da9b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-23 08:05:55 +0530

    Update antiflood.py

--- VegetaRobot/modules/antiflood.py
+++ VegetaRobot/modules/antiflood.py
@@ -354,7 +354,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "ғʟᴏᴏᴅ"
+__mod_name__ = "flood"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood)
