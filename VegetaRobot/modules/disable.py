adae28bef17163904c803916236940ab684065ba
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-23 08:43:13 +0530

    Update disable.py

--- VegetaRobot/modules/disable.py
+++ VegetaRobot/modules/disable.py
@@ -344,7 +344,7 @@ if is_module_loaded(FILENAME):
     ✪︎ `/listcmds`*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "ᴅɪsᴀʙʟᴇ"
+    __mod_name__ = "Disable"
 
 else:
     DisableAbleCommandHandler = CommandHandler
