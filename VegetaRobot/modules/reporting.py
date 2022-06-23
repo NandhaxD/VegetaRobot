48a26ed2b7e4d887171d87f270e7d57634e76bf5
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-23 11:57:59 +0530

    Update reporting.py

--- VegetaRobot/modules/reporting.py
+++ VegetaRobot/modules/reporting.py
@@ -274,6 +274,6 @@ dispatcher.add_handler(SETTING_HANDLER)
 dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
 
-__mod_name__ = "⚠Report"
+__mod_name__ = "Report"
 __handlers__ = [(REPORT_HANDLER, REPORT_GROUP),
                 (ADMIN_REPORT_HANDLER, REPORT_GROUP), (SETTING_HANDLER)]
