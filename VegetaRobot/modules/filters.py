05c090dcb6d39724d1da782857666c904fa7fbc1
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-23 08:47:46 +0530

    Update filters.py

--- VegetaRobot/modules/filters.py
+++ VegetaRobot/modules/filters.py
@@ -631,7 +631,7 @@ doin?
 Check /markdownhelp to know more!
 """
 
-__mod_name__ = "ғɪʟᴛᴇʀ"
+__mod_name__ = "Filters"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
