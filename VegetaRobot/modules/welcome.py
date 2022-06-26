83faebd78e190d2ecc490f6262678a68a3089cc5
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-26 23:45:44 +0530

    Update welcome.py

--- VegetaRobot/modules/welcome.py
+++ VegetaRobot/modules/welcome.py
@@ -289,7 +289,7 @@ def new_member(update: Update, context: CallbackContext):
                             f"{first_name} {new_mem.last_name}")
                     else:
                         fullname = escape_markdown(first_name)
-                    count = chat.get_members_count()
+                    count = chat.get_member_count()
                     mention = mention_markdown(new_mem.id,
                                                escape_markdown(first_name))
                     if new_mem.username:
