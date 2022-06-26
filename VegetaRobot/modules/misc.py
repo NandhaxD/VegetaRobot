26dbb655d0ebccb1747b049d59eb4bdd00e9224c
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-26 19:53:51 +0530

    Update misc.py

--- VegetaRobot/modules/misc.py
+++ VegetaRobot/modules/misc.py
@@ -32,7 +32,6 @@ Keep in mind that your message <b>MUST</b> contain some text other than just a b
 """
 
 
-@run_async
 @user_admin
 def echo(update: Update, context: CallbackContext):
     args = update.effective_message.text.split(None, 1)
@@ -62,7 +61,6 @@ def markdown_help_sender(update: Update):
         "[button2](buttonurl://google.com:same)")
 
 
-@run_async
 def markdown_help(update: Update, context: CallbackContext):
     if update.effective_chat.type != "private":
         update.effective_message.reply_text(
@@ -96,11 +94,11 @@ Example:
  `/cash 1 usd inr`
 Output: `1.0 USD = 75.505 INR`
 
-Reports bugs at @TangentChats
+Reports bugs at @VegetaSupport
 """
 
-ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.group)
-MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help)
+ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.group,run_async=True)
+MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help,run_async=True)
 
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
