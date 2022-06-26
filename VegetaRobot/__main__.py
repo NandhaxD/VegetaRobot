70ff25ec68918740fe46b6e1904556cab9f0006b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-26 23:50:32 +0530

    Update __main__.py

--- VegetaRobot/__main__.py
+++ VegetaRobot/__main__.py
@@ -742,7 +742,7 @@ def main():
 
     else:
         LOGGER.info("Vegeta is now alive and functioning")
-        updater.start_polling(timeout=15, read_latency=4, clean=True)
+        updater.drop_pending_updates(timeout=15, read_latency=4, clean=True)
 
     if len(argv) not in (1, 3, 4):
         telethn.disconnect()
