172bbb7608b2821520fa127dd5c945862e487951
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+Ctzfamily@users.noreply.github.com>
2022-06-27 15:42:14 +0530

    Update reporting.py

--- VegetaRobot/modules/reporting.py
+++ VegetaRobot/modules/reporting.py
@@ -263,7 +263,7 @@ __help__ = """
 
 SETTING_HANDLER = CommandHandler("reports", report_setting,run_async=True)
 REPORT_HANDLER = CommandHandler("report", report, filters=Filters.group,run_async=True)
-ADMIN_REPORT_HANDLER = MessageHandler(Filters.regex(r"(?i)@admin(s)?"), report,run_async=True)
+ADMIN_REPORT_HANDLER = MessageHandler(Filters.regex(r"(?i)@admin(s)?"), report)
 
 REPORT_BUTTON_USER_HANDLER = CallbackQueryHandler(buttons, pattern=r"report_")
 dispatcher.add_handler(REPORT_BUTTON_USER_HANDLER)
