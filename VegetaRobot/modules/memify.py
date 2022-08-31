0cb1848a79a2a17fcb06bcaff1308b9ec33c7ca4
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-08-31 19:24:43 +0530

    Update memify.py

--- VegetaRobot/modules/memify.py
+++ VegetaRobot/modules/memify.py
@@ -77,7 +77,7 @@ async def drawText(image_path, text):
 
     else:
 
-        fnt = "./VegetaRobot/images/LOGOS/logofont.otf"
+        fnt = "./VegetaRobot/resources/LOGOS/logofont.otf"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
