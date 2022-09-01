b8e4a6cc2057ef893ec0530b9ded9878dfbe4d04
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-01 05:36:27 +0530

    Update userinfo.py

--- VegetaRobot/modules/userinfo.py
+++ VegetaRobot/modules/userinfo.py
@@ -32,7 +32,7 @@ from VegetaRobot.modules.helper_funcs.chat_status import sudo_plus
 from VegetaRobot.modules.helper_funcs.extraction import extract_user
 from VegetaRobot import telethn as tbot , TIGERS, DRAGONS, DEMONS
 
-def kill(updat, context):
+def kill(update: Update, context: CallbackContext):
      message = update.effective_message
      api = requests.get("https://api.waifu.pics/sfw/kill").json()
      url = api["url"]
