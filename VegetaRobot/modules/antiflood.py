5f7db330b6f2a4c48875ed869abb87f9c061f7ff
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-03-30 15:27:34 +0530

    Update antiflood.py

--- VegetaRobot/modules/antiflood.py
+++ VegetaRobot/modules/antiflood.py
@@ -352,13 +352,13 @@ will result in restricting that user.
 __mod_name__ = "Flood"
 
 FLOOD_BAN_HANDLER = MessageHandler(
-    Filters.all & ~Filters.status_update & Filters.group, check_flood,run_async=True)
-SET_FLOOD_HANDLER = CommandHandler("setflood", set_flood, filters=Filters.group,run_async=True)
+    Filters.all & ~Filters.status_update & Filters.ChatType.GROUP, check_flood,run_async=True)
+SET_FLOOD_HANDLER = CommandHandler("setflood", set_flood, filters=Filters.ChatType.GROUP,run_async=True)
 SET_FLOOD_MODE_HANDLER = CommandHandler(
-    "setfloodmode", set_flood_mode, pass_args=True,run_async=True)  #, filters=Filters.group)
+    "setfloodmode", set_flood_mode, pass_args=True,run_async=True, filters=Filters.ChatType.GROUP)
 FLOOD_QUERY_HANDLER = CallbackQueryHandler(
     flood_button, pattern=r"unmute_flooder")
-FLOOD_HANDLER = CommandHandler("flood", flood, filters=Filters.group,run_async=True)
+FLOOD_HANDLER = CommandHandler("flood", flood, filters=Filters.ChatType.GROUP,run_async=True)
 
 dispatcher.add_handler(FLOOD_BAN_HANDLER, FLOOD_GROUP)
 dispatcher.add_handler(FLOOD_QUERY_HANDLER)
