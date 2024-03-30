9f06606c01e2da67581ea158f884e724500e854e
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-03-30 14:20:45 +0530

    Update modules.py

--- VegetaRobot/modules/modules.py
+++ VegetaRobot/modules/modules.py
@@ -10,7 +10,6 @@ from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
 
-@run_async
 @dev_plus
 def load(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -80,7 +79,6 @@ def load(update: Update, context: CallbackContext):
         parse_mode=ParseMode.HTML)
 
 
-@run_async
 @dev_plus
 def unload(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -152,7 +150,6 @@ def unload(update: Update, context: CallbackContext):
         parse_mode=ParseMode.HTML)
 
 
-@run_async
 @sudo_plus
 def listmodules(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -168,9 +165,9 @@ def listmodules(update: Update, context: CallbackContext):
     message.reply_text(module_list, parse_mode=ParseMode.HTML)
 
 
-LOAD_HANDLER = CommandHandler("load", load)
-UNLOAD_HANDLER = CommandHandler("unload", unload)
-LISTMODULES_HANDLER = CommandHandler("listmodules", listmodules)
+LOAD_HANDLER = CommandHandler("load", load, run_async=True)
+UNLOAD_HANDLER = CommandHandler("unload", unload, run_async=True)
+LISTMODULES_HANDLER = CommandHandler("listmodules", listmodules, run_async=True)
 
 dispatcher.add_handler(LOAD_HANDLER)
 dispatcher.add_handler(UNLOAD_HANDLER)
