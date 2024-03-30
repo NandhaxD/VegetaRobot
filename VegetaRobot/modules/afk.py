3a6f141a3675ea0b982c98051c72bc1c5eda777f
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-03-30 15:21:44 +0530

    Update afk.py

--- VegetaRobot/modules/afk.py
+++ VegetaRobot/modules/afk.py
@@ -134,8 +134,8 @@ def check_afk(update, context, user_id, fst_name, userc_id):
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk,run_async=True)
 AFK_REGEX_HANDLER = DisableAbleMessageHandler(
     Filters.regex(r"^(?i)brb(.*)$"), afk, friendly="afk",run_async=True)
-NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk,run_async=True)
-AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group, reply_afk,run_async=True)
+NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.ChatType.GROUP, no_longer_afk,run_async=True)
+AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.ChatType.GROUP, reply_afk,run_async=True)
 
 dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
