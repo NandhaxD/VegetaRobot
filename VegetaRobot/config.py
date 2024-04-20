fb7554a7b60f622d29b65780e279c0f8a264be50
iAmShashiVarma404 <117093915+iAmShashiVarma404@users.noreply.github.com>
2024-04-20 18:20:21 +0530

    Update config.py

--- VegetaRobot/config.py
+++ VegetaRobot/config.py
@@ -21,13 +21,13 @@ class Config(object):
     API_HASH = "awew"
     TOKEN = "wow"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     SQLALCHEMY_DATABASE_URI = "" #Use Your ElephantSQL
-    OWNER_ID = 1491497760  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 5781844850  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "nandhaxd"
     SUPPORT_CHAT = 'vegetasupport'  #Your own group for support, do not add the @
     UPDATES_CHANNEL = 'vegetaUpdates' #Your own channel for Updates of bot, Do not add @
-    JOIN_LOGGER = -1001543354286  #Prints any new group the bot is added to, prints just the name and ID.
+    JOIN_LOGGER = 7179142363  #Prints any new group the bot is added to, prints just the name and ID.
     REM_BG_API_KEY = "dxsh728mZMDmj4ijSZCNPZig"
-    EVENT_LOGS = -1001543354286  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    EVENT_LOGS = 7179142363  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     LOAD = []
     NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
     WEBHOOK = None
