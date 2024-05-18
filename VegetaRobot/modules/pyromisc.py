e84ffdb94ed7cb09385e6e03b2cef15659a3ebd3
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-18 17:59:09 +0530

    Update pyromisc.py
    Ud update

--- VegetaRobot/modules/pyromisc.py
+++ VegetaRobot/modules/pyromisc.py
@@ -6,21 +6,82 @@ from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeybo
 from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
 from pyrogram.types import Message
 from gpytranslate import Translator
-from VegetaRobot import pgram as bot, SUPPORT_CHAT, arq
+from VegetaRobot import pgram as bot, pgram as app, SUPPORT_CHAT, arq
 from urllib.parse import quote
 
        
-@bot.on_message(filters.command("ud"))
-async def ud(_, message: Message):
-        if len(message.command) < 2:
-             await message.reply("gime a ward")
-             return
-        text = message.text.split(None, 1)[1]
-        results = requests.get(
-        f'https://api.urbandictionary.com/v0/define?term={text}').json()
-        reply_text = f'**results: {text}**\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
-        ud = await message.reply_text("finding.. define.")
-        await ud.edit_text(reply_text)
+
+
+
+#By @NandhaBots on telegram 
+
+@app.on_message(filters.command("ud"))
+async def urban(_, m):  
+       user_id = m.from_user.id
+       if len(m.text.split()) == 1:
+         return await m.reply("Enter the text for which you would like to find the definition.")
+       text = m.text.split(None,1)[1]
+       api = requests.get(f"https://api.urbandictionary.com/v0/define?term={text}").json()
+       mm = api["list"]
+       if 0 == len(mm):
+           return await m.reply("=> No results Found!")
+       string = f"🔍 **Ward**: {mm[0].get('word')}\n\n📝 **Definition**: {mm[0].get('definition')}\n\n✏️ **Example**: {mm[0].get('example')}"
+       if 1 == len(mm):
+           return await m.reply(text=string, quote=True)
+       else:
+           num = 0
+           return await m.reply(text=string, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('next', callback_data=f"udnxt:{user_id}:{text}:{num}")]]), quote=True)
+              
+@app.on_callback_query(filters.regex("^udnxt"))   
+async def next(_, query):
+         user_id = int(query.data.split(":")[1])
+         text = str(query.data.split(":")[2])
+         num = int(query.data.split(":")[3])+1
+         if not query.from_user.id == user_id:
+             return await query.answer("This is not for You!")
+         api = requests.get(f"https://api.urbandictionary.com/v0/define?term={text}").json()
+         mm = api["list"]
+         uwu = mm[num]
+         if num == len(mm)-1:
+             string = f"🔍 **Ward**: {uwu.get('word')}\n\n📝 **Definition**: {uwu.get('definition')}\n\n✏️ **Example**: {uwu.get('example')}\n\n"
+             string += f"Page: {num+1}/{len(mm)}"
+             return await query.message.edit(text=string, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('➡️ Back', callback_data=f"udbck:{query.from_user.id}:{text}:{num}")]]))
+         else:
+             string = f"🔍 **Ward**: {uwu.get('word')}\n\n📝 **Definition**: {uwu.get('definition')}\n\n✏️ **Example**: {uwu.get('example')}\n\n"
+             string += f"Page: {num+1}/{len(mm)}"
+             buttons = [[
+                  InlineKeyboardButton("Back ⏮️", callback_data=f"udbck:{query.from_user.id}:{text}:{num}"),
+                  InlineKeyboardButton("Next ⏭️", callback_data=f"udnxt:{query.from_user.id}:{text}:{num}") 
+             ]]
+             return await query.message.edit(text=string, reply_markup=InlineKeyboardMarkup(buttons))
+
+@app.on_callback_query(filters.regex("^udbck"))   
+async def back(_, query):
+         user_id = int(query.data.split(":")[1])
+         text = str(query.data.split(":")[2])
+         num = int(query.data.split(":")[3])-1
+         if not query.from_user.id == user_id:
+             return await query.answer("This is not for You!")
+         api = requests.get(f"https://api.urbandictionary.com/v0/define?term={text}").json()
+         mm = api["list"]
+         uwu = mm[num]
+         if num == 0:
+             string = f"🔍 **Ward**: {uwu.get('word')}\n\n📝 **Definition**: {uwu.get('definition')}\n\n✏️ **Example**: {uwu.get('example')}\n\n"
+             string += f"Page: {num+1}/{len(mm)}"
+             return await query.message.edit(text=string, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('➡️ Next', callback_data=f"udnxt:{query.from_user.id}:{text}:{num}")]]))
+         else:
+             string = f"🔍 **Ward**: {uwu.get('word')}\n\n📝 **Definition**: {uwu.get('definition')}\n\n✏️ **Example**: {uwu.get('example')}\n\n"
+             string += f"Page: {num+1}/{len(mm)}"
+             buttons = [[
+                  InlineKeyboardButton("Back ⏮️", callback_data=f"udbck:{query.from_user.id}:{text}:{num}"),
+                  InlineKeyboardButton("Next ⏭️", callback_data=f"udnxt:{query.from_user.id}:{text}:{num}") 
+             ]]
+             return await query.message.edit(text=string, reply_markup=InlineKeyboardMarkup(buttons))
+       
+       
+       
+
+
 
 @bot.on_message(filters.command("spell"))
 async def spellcheck(_, m):
