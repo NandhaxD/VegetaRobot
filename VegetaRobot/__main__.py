b5480290140419f72eaceb8e48e0ade405804216
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-26 20:13:55 +0530

    Update __main__.py

--- VegetaRobot/__main__.py
+++ VegetaRobot/__main__.py
@@ -81,14 +81,14 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-PM_START_TEXT = """ ┗►  *What's Up Yoo! * ◄┛
+PM_START_TEXT = """ *┗► What's Up, Earthling! ◄┛
 
-~~ *I'm made with Saiyans blood* ~~
+~~ I am the Prince of All Saiyans, Vegeta! ~~
 
-~ *Wanna see my commands*[❗]({})
-*below click the help button!* ~
-
-──『*ᴛʜᴀɴᴋs  ғᴏʀ  ᴜsɪɴɢ*』
+~ Prepare yourself for my commands! ~
+Click the help button below if you dare!
+[\u200]({})
+──『 *You better be ready to train hard!* 』──
 """ 
 
 buttons = [
@@ -233,7 +233,8 @@ def start(update: Update, context: CallbackContext):
     else:
         first_name = update.effective_user.first_name
         update.effective_message.reply_animation(
-            GROUPSTART_IMG, caption= "*hello!\n ┗► {} ◄┛,*\n*Super Saiyan here*\n*Power lavel time* : {} ".format(
+             GROUPSTART_IMG, 
+             caption="*Greetings!\n ┗► {} ◄┛,\nSaiyan Warrior reporting\nEnergy level now : {} *".format(
              first_name,uptime
             ),
             parse_mode=ParseMode.MARKDOWN,
@@ -664,7 +665,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}","[SUPER SAIYAN VEGETA IS BACK](https://telegra.ph/file/d3db0babad0d1729c5f59.jpg)", parse_mode=ParseMode.MARKDOWN) 
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}","[PRINCE VEGETA RETURNS](https://telegra.ph/file/d3db0babad0d1729c5f59.jpg)", parse_mode=ParseMode.MARKDOWN) 
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!",
@@ -710,7 +711,7 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN)
 
     else:
-        LOGGER.info("Vegeta is now alive and functioning")
+        LOGGER.info("Vegeta Is Now Alive And Functioning")
         updater.start_polling(timeout=15, read_latency=4, drop_pending_updates=True)
         if len(argv) not in (1, 3, 4):
             telethn.disconnect()
