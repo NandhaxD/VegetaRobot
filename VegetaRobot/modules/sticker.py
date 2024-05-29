200b427a20dca71b04f9403f9459e4d6bab7c66c
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-29 19:44:40 +0530

    Update sticker.py

--- VegetaRobot/modules/sticker.py
+++ VegetaRobot/modules/sticker.py
@@ -823,21 +823,14 @@ def video(update: Update, context: CallbackContext):
 async def handler(event):
     if event.fwd_from:
         return
-
     if not event.reply_to_msg_id:
         await event.reply("Provide Some Text To Draw!")
-
         return
-
     reply_message = await event.get_reply_message()
-
     if not reply_message.media:
         await event.reply("```ʀᴇᴘʟʏ ᴛᴏ ᴀ ɪᴍᴀɢᴇ/sᴛɪᴄᴋᴇʀ.```")
-
         return
-
     file = await bot.download_media(reply_message)
-
     msg = await event.reply("```ᴍᴇᴍɪғʏɪɴɢ ᴛʜɪs ɪᴍᴀɢᴇ! 😉 ```")
 
     # if "Abishnoi69" in Credit:
@@ -847,49 +840,33 @@ async def handler(event):
     #     await event.reply("ᴛʜɪs ɴɪɢɢᴀ ʀᴇᴍᴏᴠᴇᴅ ᴄʀᴇᴅɪᴛ ʟɪɴᴇ ғʀᴏᴍ ᴄᴏᴅᴇ 😶")
 
     text = str(event.pattern_match.group(1)).strip()
-
     if len(text) < 1:
         return await msg.reply("ʏᴏᴜ ᴍɪɢʜᴛ ᴡᴀɴᴛ ᴛᴏ ᴛʀʏ `/mmf text`")
-
     meme = await drawText(file, text)
-
     await bot.send_file(event.chat_id, file=meme, force_document=False)
-
     await msg.delete()
-
     os.remove(meme)
-
+  
 
 async def drawText(image_path, text):
     img = Image.open(image_path)
-
     os.remove(image_path)
-
+    shadowcolor = "black"
     i_width, i_height = img.size
-
-    if os.name == "nt":
-        fnt = "ariel.ttf"
-
-    else:
-        fnt = "./Exon/modules/resources/asu.ttf"
-
+    fnt = "./VegetaRobot/resources/LOGOS/logofont.otf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
-
     if ";" in text:
         upper_text, lower_text = text.split(";")
-
     else:
         upper_text = text
-
         lower_text = ""
-
     draw = ImageDraw.Draw(img)
-
     current_h, pad = 10, 5
-
     if upper_text:
         for u_text in textwrap.wrap(upper_text, width=15):
-            u_width, u_height = draw.textsize(u_text, font=m_font)
+            u_bbox = draw.textbbox((0, 0), u_text, font=m_font)
+            u_width = u_bbox[2] - u_bbox[0]
+            u_height = u_bbox[3] - u_bbox[1]
 
             draw.text(
                 xy=(((i_width - u_width) / 2) - 2, int((current_h / 640) * i_width)),
@@ -897,41 +874,38 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(0, 0, 0),
             )
-
             draw.text(
                 xy=(((i_width - u_width) / 2) + 2, int((current_h / 640) * i_width)),
                 text=u_text,
                 font=m_font,
                 fill=(0, 0, 0),
             )
-
             draw.text(
                 xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 2),
                 text=u_text,
                 font=m_font,
                 fill=(0, 0, 0),
             )
-
             draw.text(
                 xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 2),
                 text=u_text,
                 font=m_font,
                 fill=(0, 0, 0),
             )
-
             draw.text(
                 xy=((i_width - u_width) / 2, int((current_h / 640) * i_width)),
                 text=u_text,
                 font=m_font,
                 fill=(255, 255, 255),
             )
-
             current_h += u_height + pad
 
     if lower_text:
         for l_text in textwrap.wrap(lower_text, width=15):
-            u_width, u_height = draw.textsize(l_text, font=m_font)
-
+            u_bbox = draw.textbbox((0, 0), l_text, font=m_font)
+            u_width = u_bbox[2] - u_bbox[0]
+            u_height = u_bbox[3] - u_bbox[1]
+      
             draw.text(
                 xy=(
                     ((i_width - u_width) / 2) - 2,
@@ -981,16 +955,13 @@ async def drawText(image_path, text):
                 font=m_font,
                 fill=(255, 255, 255),
             )
-
             current_h += u_height + pad
-
+          
     image_name = "memify.webp"
-
     webp_file = os.path.join(image_name)
-
     img.save(webp_file, "webp")
-
     return webp_file
+
 
 
 __mod_name__ = "Sticker"
