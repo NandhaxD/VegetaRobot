590cdf0a8ac1c7d50bd0aa81abaf446cabbb7e0e
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-30 13:43:25 +0530

    Update blstickers.py

--- VegetaRobot/modules/blstickers.py
+++ VegetaRobot/modules/blstickers.py
@@ -514,10 +514,11 @@ UNBLACKLIST_STICKER_HANDLER = CommandHandler(
 )
 
 BLACKLISTMODE_HANDLER = CommandHandler(
-  "blstickermode", blacklist_mode, run_async=True)
+  "blstickermode", blacklist_mode, run_async=True
+)
 
 BLACKLIST_STICKER_DEL_HANDLER = MessageHandler(
-    Filters.sticker & Filters.group, del_blackliststicker, run_async=True
+    Filters.sticker & Filters.chat_type.groups, del_blackliststicker, run_async=True
 )
 
 dispatcher.add_handler(BLACKLIST_STICKER_HANDLER)
