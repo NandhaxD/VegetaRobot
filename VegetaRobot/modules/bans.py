28009e84c7242a5c9e2e25cf50c200092c230628
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-02 18:48:16 +0530

    Update bans.py

--- VegetaRobot/modules/bans.py
+++ VegetaRobot/modules/bans.py
@@ -64,14 +64,8 @@ def ban(update: Update, context: CallbackContext) -> str:
     user_id, reason = extract_user_and_text(message, args)
 
     if message.reply_to_message and message.reply_to_message.sender_chat:
-      
-        r = bot._request.post(bot.base_url + '/banChatSenderChat', {
-            'sender_chat_id': message.reply_to_message.sender_chat.id,
-            'chat_id': chat.id
-            })
-        sender_chat = message.reply_to_message.sender_chat
-      
-        text = f"*Successfully banned {html.escape(sender_chat.title)} from {html.escape(message.chat.title)}*"      
+        sender_chat = message.reply_to_message.sender_chat  
+        text = f"*You can't ban a channel using this command but you can use my /cban instead.*"      
       
         return message.reply_text(
             text=text, 
