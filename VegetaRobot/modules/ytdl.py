fba947dd0818c58cdac957a575783b5294ff4f29
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-18 23:19:37 +0530

    Create ytdl.py

---
+++ VegetaRobot/modules/ytdl.py
@@ -0,0 +1,56 @@
+
+import re
+from pytube import YouTube
+
+
+from pyrogram import filters, types, enums, errors
+from VegetaRobot import pgram
+
+
+pattern = r'(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\w{11})|(?:youtu\.be\/\w{11}))(?:\S+)?'
+
+
+def is_youtube_url(text: str):
+    match = re.search(pattern, text)
+    url = None
+    if match:
+        url = match.group(0)
+    return url
+
+
+
+def download(link: str, mode: str):
+    yt = YouTube(url)
+    if mode == "ytaudio":
+        file = yt.streams.get_audio_only()
+    else:
+        file = yt.streams.get_highest_resolution()
+    path = file.download('./')
+    return [ yt.title, path ]
+
+
+
+@pgram.on_message(filters.command(['ytaudio','ytvideo']))
+async def youtube_dl(bot, m:= types.message):
+       cmd = m.command[0]
+       if not len(m.text.split()) >= 2:
+            return await m.reply_text("```Example:\n/ytvideo url```")
+       else:
+          link = is_youtube_url(m.text)
+          if not link:
+              return await m.reply_text("🤔 not youtube link")
+          else:
+              msg = await m.reply_text("✨ Downloading....")
+              try:
+                 title, path = download(link, cmd)
+              except Exception as e:
+                  return await msg.edit(f"❌ Error: {e}")
+              if (await m.reply_document(
+                   document=path, caption=title, force_document=False, quote=True
+              )):
+                   await msg.delete()
+                  
+             
+        
+
+
