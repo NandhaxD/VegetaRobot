b153f7b3d26d79545666f9ba8d621e0f63224488
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-29 10:48:17 +0530

    Update instadl.py

--- VegetaRobot/modules/instadl.py
+++ VegetaRobot/modules/instadl.py
@@ -12,6 +12,9 @@ https://hoshi-api-f62i.onrender.com/
 """
 
 
+
+
+
 @pbot.on_message(filters.command(["insta","ig", "instadl"]))
 async def insta_download(client, message):
     try:
@@ -19,6 +22,7 @@ async def insta_download(client, message):
     except IndexError:
         return await message.reply("Provide URL. Usage: /insta [url]")
 
+    
     chat_id = message.chat.id
 
     try:
@@ -26,32 +30,46 @@ async def insta_download(client, message):
         if response.status_code != 200:
             return await message.reply(f"Error fetching Instagram data. Status code: {response.status_code}\nReport @{SUPPORT_CHAT}")
 
+        msg = await message.reply_text(
+        text="🔍 Fetching media's please wait...."
+        )
+      
         data = response.json()
         media_list = data.get("result", [])
 
         if url.startswith("https://www.instagram.com/p/"):
             media_group = []
             for media_url in media_list[:8]:
-                media_group.append(InputMediaPhoto(media=media_url))
+                media_group.append(
+                  InputMediaPhoto(media=media_url)
+                )
 
-            lalal = await message.reply(f"=> Fetched {len(media_group)} Media")
-            await pbot.send_media_group(chat_id=chat_id, media=media_group)
-            await lalal.delete()
+            await msg.edit_text(f"⚡ Successfully Fetched {len(media_group)} Media")
+            ok = await pbot.send_media_group(
+              chat_id=chat_id, 
+              media=media_group
+            )
+            if ok: return await msg.delete()
 
         elif url.startswith("https://www.instagram.com/reel/"):
             for video_url in media_list:
-                await pbot.send_video(chat_id=chat_id, video=video_url)
+                ok = await pbot.send_video(
+                  chat_id=chat_id,
+                  video=video_url
+                )
+                if ok: return await msg.delete()
 
         elif url.startswith("https://www.instagram.com/stories/"):
+            await msg.edit("Uploading stories ⚡")
             for story_url in media_list:
                 try:
                     await pbot.send_video(chat_id=chat_id, video=story_url)
-                    await asyncio.sleep(0.3)
+                    await asyncio.sleep(0.5)
                 except Exception as e:
-                    await message.reply(f"Error sending video: {e}")
-
+                    await message.reply_text(f"{story_url}: Error sending video: {e}")
+            await msg.delete()
         else:
-            await message.reply("Invalid Instagram link")
+            await msg.edit_text("Invalid Instagram link")
 
     except Exception as e:
-        await message.reply(f"Error: {e}")
+        await msg.edit(f"Error: {e}")
